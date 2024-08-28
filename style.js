0b6106de5ae05023c64d88b8e692c7a20a75d9b0
Totita <totita.olivia@gmail.com>
2024-08-27 23:22:41 -0400

    Creando

---
+++ style.js
@@ -0,0 +1,17 @@
+*{
+    background: "#00808";
+    font-family: "Baskervville SC";
+    font-weight: 400;
+    font-size: 32px;
+    line-height: 150%
+}
+
+.Logo{
+    padding-top: 10px;
+    margin-left: 10px;
+    width: 10px
+}
+
+main{
+    display: flex;
+}
