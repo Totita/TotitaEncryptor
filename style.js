33876c705acb8189ae2e23ce2326202f41b743e7
Totita <totita.olivia@gmail.com>
2024-08-27 23:54:15 -0400

    seguimos

--- style.js
+++ style.js
@@ -9,11 +9,27 @@
 .Logo{
     padding-top: 10px;
     margin-left: 10px;
-    width: 10px
+    width: 10px;
 }
 
 main {
     display: flex;
     margin-botton: 50px
     margin-left: 80px
+}
+
+.text-area {
+    border: none;
+    color: #08080;
+    margin-top: 90px;
+    text-transform: lowercase;
+
+}
+
+placeholder{ color: #800080;}
+    .text-area: focus{
+        outline: none
+    }
+
+
 }
